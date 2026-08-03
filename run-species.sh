b5248db38d76f890316c5155db76de9b0d929666
Hyunsu Lim <karegoie@snu.ac.kr>
2026-08-03 15:34:58 +0900

    added ./time

--- run-species.sh
+++ run-species.sh
@@ -1,1 +1,1 @@
-echo "cd `pwd`; ./plantsds/run_sd.sh -t 128 -s t2t-nip" | qsub -N plantsds-species -l nodes=node02:ppn=128 -v WORKDIR=`pwd` -j oe -o ~/log/plantsds-species.log
+echo "cd `pwd`; ./time ./plantsds/run_sd.sh -t 128 -s t2t-nip" | qsub -N plantsds-species -l nodes=node02:ppn=128 -v WORKDIR=`pwd` -j oe -o ~/log/plantsds-species.log
