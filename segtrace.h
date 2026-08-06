8275a24be00c77865f78851fc3020443ed59a587
Hyunsu Lim <thehyunsulim@gmail.com>
2026-08-06 11:40:03 +0900

    max_run_len

--- segtrace.h
+++ segtrace.h
@@ -49,7 +49,7 @@ extern "C" {
 #define ABS_DIFF(a, b) ((a) > (b) ? (a) - (b) : (b) - (a))
 
 #define NUM_PARTITIONS 1024
-#define MAX_RUN_LEN 100
+#define MAX_RUN_LEN 1000
 
 #define BLOOM_SIZE_BITS (1 << 30)
 #define BLOOM_SIZE_BYTES (BLOOM_SIZE_BITS / 8)
